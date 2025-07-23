ac03c0a6d137d3a334f08f2fdf9a7f6f8a2f1b90
naeunmin <babaleo115@gmail.com>
2025-07-23 14:37:15 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/likelionapp/LikelionAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionAppApplication.class, args);
+    }
+
+}
